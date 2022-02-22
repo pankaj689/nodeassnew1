56a304a024d6d4a7ecb3d0c56cbae6fc4fa7e06c
pankaj patil <patil1995.pankaj@gmail.com>
2022-02-22 20:30:48 +0530

    initial comitevforass1

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,23 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    return process.argv[process.argv.length-1]
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name
 }
 
 function getNameFromReadLine() {
-    // Write your code here
+    // Write your code hererw
+    var namecom=require("readline");
+    var ipt =namecom.createInterface({
+        input:process.stdip,
+        output:process.stdout
+    })
+    ipt.question("nmae",(ans)=>{
+        console.log(ans)
+    })
 }
 
 module.exports = {
